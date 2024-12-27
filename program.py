10cfdc73d261bca377408f44185c07dcf54de313
guanshiyin28 <guanshiyinnn@gmail.com>
2024-12-27 07:23:45 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,19 @@
+# Delete First Function
+
+arr = [1, 2, 3, 4, 5]
+
+print("Array before deleted:", arr)
+
+arr.pop(0)
+
+print("\nArray after deleted:", arr)
+
+# Delete Last Function
+
+arr = [1, 2, 3, 4, 5]
+
+print("Array before deleted:", arr)
+
+arr.pop()
+
+print("\nArray after deleted:", arr)
